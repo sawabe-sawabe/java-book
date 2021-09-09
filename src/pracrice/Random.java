f9049583a2d6dc3bb0d3d20fc3a5a2095e68b6dd
sawabe-sawabe <kapo0515.0515@gmail.com>
2021-09-09 23:34:08 +0900

    practice

--- src/pracrice/Random.java
+++ src/pracrice/Random.java
@@ -4,7 +4,7 @@ public class Random {
   public static void main(String[] args) {
 	  System.out.println("あなたの名前を教えてください");
 	  String name = new java.util.Scanner(System.in).nextLine();
-	  System.out.println("あなたの年齢を教えてください");
+	  System.out.println("あなたの歳を教えてください");
 	  int age = new java.util.Scanner(System.in).nextInt();
 	  int r = new java.util.Random().nextInt(100);
 
