f3373fafd65be00bf7c841fc62341662ef592f5f
sawabe-sawabe <kapo0515.0515@gmail.com>
2021-09-12 23:35:06 +0900

    practice

---
+++ src/pracrice/practiceMax.java
@@ -0,0 +1,12 @@
+package pracrice;
+
+public class practiceMax {
+	  public static void main(String[] args) {
+		int mika=  23;
+		int maki =24;
+		int m = Math.max(mika,maki);
+		System.out.println("マキとみか年上はマキで"+m+"さいです");
+	}
+
+
+}
