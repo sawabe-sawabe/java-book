ff108b94305ee54178d8151892da218f50835840
sawabe-sawabe <kapo0515.0515@gmail.com>
2021-09-21 22:38:42 +0900

    practice

---
+++ src/pracrice/score.java
@@ -0,0 +1,11 @@
+package pracrice;
+
+public class score {
+
+	public static void main(String[] args) {
+		int [] scores = new int [5];
+		int num = scores.length;
+		System.out.println("要素の数:"+num);
+
+	}
+}
