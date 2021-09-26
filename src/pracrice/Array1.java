e8d925db96c823f8a211ac45adf4436119ac2f01
sawabe-sawabe <kapo0515.0515@gmail.com>
2021-09-27 06:59:34 +0900

    practice

---
+++ src/pracrice/Array1.java
@@ -0,0 +1,11 @@
+package pracrice;
+
+public class Array1 {
+	public static void main(String[] args) {
+		int[] scores = {20,30,40,50,80};
+		for(int i = 0 ;i < scores.length; i++) {
+			System.out.println(scores[i]);
+		}
+	}
+
+}
