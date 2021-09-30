24ff20cc6c8451d48fa6f1c1121e45c9e5b3e059
sawabe-sawabe <kapo0515.0515@gmail.com>
2021-09-30 20:00:23 +0900

    practice

--- src/pracrice/Array1.java
+++ src/pracrice/Array1.java
@@ -3,9 +3,18 @@ package pracrice;
 public class Array1 {
 	public static void main(String[] args) {
 		int[] scores = {20,30,40,50,80};
+		int sum = 0;
+		int count =0;
 		for(int i = 0 ;i < scores.length; i++) {
-			System.out.println(scores[i]);
+			if(scores[i] >= 50) {
+				count++;
+			}
+			sum += scores[i];
 		}
+		System.out .println("50点以上の科目の数は"+count);
+		int avg = sum/ scores.length;
+		System.out .println("合計点"+sum);
+		System.out .println("平均点"+avg);
 	}
 
 }
