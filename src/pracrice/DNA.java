ffa4498e69e2c342170039b0d28e0c6994d0c348
sawabe-sawabe <kapo0515.0515@gmail.com>
2021-10-01 16:41:47 +0900

    practice

---
+++ src/pracrice/DNA.java
@@ -0,0 +1,17 @@
+package pracrice;
+
+public class DNA {
+	public static void main(String[] args) {
+		int [] seq = new int[10];
+
+		for(int i = 0; i<seq.length;i ++) {
+			seq[i] = new java.util.Random().nextInt(4);
+		}
+
+		for(int i = 0; i<seq.length;i ++) {
+		  char[] base = {'A','T','G','C'};
+		  System.out.println(base[seq[i]]);
+		}
+	}
+
+}
