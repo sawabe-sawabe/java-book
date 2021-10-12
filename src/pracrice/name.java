18de5e4cdec8d1789e0f762b9ba5c2c7232cda5e
sawabe-sawabe <kapo0515.0515@gmail.com>
2021-10-12 20:18:51 +0900

    practice

--- src/pracrice/name.java
+++ src/pracrice/name.java
@@ -3,6 +3,7 @@ package pracrice;
 public class name {
 	public static void main(String[] args) {
 		hello("akira");
+		hello("助けて");
 	}
 	public static void hello(String name) {
 		System.out.println(name+"さん、こんにちは！");
